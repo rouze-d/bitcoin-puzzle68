573cc33cc2640b5e20b3054b53c3dd79b769b39d
Rouze <50823809+rouze-d@users.noreply.github.com>
2025-02-26 08:48:15 +0800

    Update and rename puzzle67.py to puzzle68.py
    change code to 68 bit puzzle

--- puzzle68.py
+++ puzzle68.py
@@ -9,7 +9,7 @@ count=0
 total=0
 
 while True:
-    num = random.randrange(73786976294838206464, 147573952589676412928)
+    num = random.randrange(147573952589676412928, 295147905179352825855)
     compress_address = ice.privatekey_to_address( 0, True, num)
     magic = hex(num)[2:].zfill(64)
 
@@ -17,7 +17,7 @@ while True:
     total+=1
     print(total, magic, compress_address, end='\r')
 
-    if compress_address == "1BY8GQbnueYofwSuFAT3USAhGjPrkxDdW9" :
+    if compress_address == "1MVDYgVaSN6iKKEsbzRUAYFrYJadLYZvvZ" :
 
         
         print('\nPrivate HEX     : ', magic)
